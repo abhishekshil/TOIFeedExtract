3b511a46bc9441fc0ac04ebc06eeffc044d6e21d
Abhishek <sil.abhishek97@gmail.com>
2018-07-11 11:06:36 +0530

    commit 2

---
+++ flask/venv/db_conn.py
@@ -0,0 +1,23 @@
+import pymysql
+from bs4 import BeautifulSoup
+import urllib.request as urllib
+import re
+import array
+#import lxml
+
+conn=pymysql.connect(host="localhost",user="root",passwd="",db="link_table")
+mycursor=conn.cursor()
+url = "https://timesofindia.indiatimes.com/rss.cms"
+page = urllib.urlopen(url)
+soup=BeautifulSoup(page,'html.parser')
+table=soup.find(lambda tag: tag.name=='table' and tag.has_attr('width') and tag['width']=="640" and tag['href']!="javascript:void(0)")
+table_links=[]
+
+for link in table.findAll('a'):
+    table_links=link.get('href') 
+    print(table_links)
+    #mycursor.execute("INSERT INTO link(link) VALUES(%s)",(table_links))
+    #mycursor.execute("DELETE FROM link where link=10;")
+
+conn.commit()
+conn.close()
